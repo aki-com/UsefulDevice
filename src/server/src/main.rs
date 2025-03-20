9480327709469a9a4279655a4ef03cce88a321b9
USlayout <masanori20170507@gmail.com>
2025-03-20 21:41:15 +0900

    asdfghjkl;

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,21 +1,13 @@
 mod server;
-
 mod device_ctrl;
 // cd src/server; cargo run
 
-
-mod audio_ctrl;
 fn main() {
-    match set_volume(0.5) {  // 50% に設定
-        Ok(_) => println!("音量を変更しました"),
-        Err(e) => eprintln!("エラー: {:?}", e),
-    };
+
 
 
     server::start_server();
-
-    audio_ctrl::adjust_volume(30);
-    audio_ctrl::parse_volume_command("Vad(75)");
+    device_ctrl::vol();
 }
 
 
