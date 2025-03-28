2954e0de9344b0026b602732bb8057f28aa49a75
aki-com <136059874+aki-com@users.noreply.github.com>
2025-03-28 15:16:09 +0900

    dialogをどうにかした

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,40 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use slint::{Model, ModelRc};
 use ud_server::server_test;
 use ud_client::{client_test, get_server};
 
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
-    server_test();
-    client_test(); 
+
+
+
+
+
+fn device_get() -> ModelRc<Device> {
     let device_raw = get_server(); //デバイスの取得
-    println!("{:?}",device_raw);//デバイスの取得
-    //Device型に変更
     let devices = slint::VecModel::from(device_raw.iter().map(|(key, (name, ip))| {
         Device {
             device_name: name.clone().into(),
             IP_address: ip.to_string().into(),
         }
     }).collect::<Vec<_>>());
-    //デバイスの最後尾を追加
-    devices.push(Device {
-        device_name: "test".into(),
-        IP_address: "test".into(),
-    });
+    slint::ModelRc::new(devices)
+}
 
-    //ui.set_name(slint::ModelRc::new(devices));
-    ui.on_show_settings(|| {
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+    ui.on_show_settings(move|| {
         let dialog = device_search::new().unwrap();
+        dialog.set_devices(device_get());
         dialog.show().unwrap();
+
+
     });
-    ui.global::<App_Data>().set_devices(slint::ModelRc::new(devices));
+    
+   // ui.set_name(slint::ModelRc::new(devices));
+//    ui.global::<App_Data>().set_devices(slint::ModelRc::new(devices));
 
     //ui.on_○○でslintのイベントを登録する
 
