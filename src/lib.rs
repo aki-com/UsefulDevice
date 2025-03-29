63101f58dc4bf74ac31076760bcdd71d434502f6
USlayout <masanori20170507@gmail.com>
2025-03-29 22:39:10 +0900

    lib.rs
    修正

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,38 @@
+slint::include_modules!();
+use slint::{Model, ModelRc};
+use std::rc::Rc;
+use ud_client::{client_test, get_server};
 
-slint::slint! {
-    export component AppWindow inherits Window {
-        Text {
-            text: "Slint & Android";
+fn device_get() -> ModelRc<Device> {
+    let device_raw = get_server(); //デバイスの取得
+    let devices = slint::VecModel::from(device_raw.iter().map(|(key, (name, ip))| {
+        Device {
+            device_name: name.clone().into(),
+            IP_address: ip.tostring().into(),
         }
-    }
+    }).collect::<Vec<>>());
+    slint::ModelRc::new(devices)
 }
 
+
 #[no_mangle]
-fn android_main(app: slint::android::AndroidApp) {
+fn android_main(app: slint::android::AndroidApp) -> Result<(), Box<dyn std::error::Error>> {
     slint::android::init(app).unwrap();
-    AppWindow::new().unwrap().run().unwrap();
+
+    let ui = Rc::new(RefCell::new(AppWindow::new()?));
+    let ui_clone = ui.clone();
+
+
+
+    ui.borrow().on_list_update(move || {
+        ui_clone.borrow().set_devices(device_get());
+        get_server();
+    });
+
+
+
+    ui.borrow().run()?;
+
+    Ok(())
+
 }
